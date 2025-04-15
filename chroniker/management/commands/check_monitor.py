8826b807e1c3d428309d99dbb63a6e32aa5ab333
Alexey Ulyanov <alex@gateway.fm>
2025-04-15 07:14:20 +0000

    Refrite check monitor to avoid eval and exec

--- chroniker/management/commands/check_monitor.py
+++ chroniker/management/commands/check_monitor.py
@@ -1,48 +1,63 @@
 import sys
-
 from django.core.management.base import BaseCommand
-
 from chroniker.models import get_current_job
+from django.apps import apps
 
 
 class Command(BaseCommand):
-    help = 'Runs a specific monitoring routine.'
+    help = "Runs a specific monitoring routine."
 
     def add_arguments(self, parser):
-        parser.add_argument('args', nargs="*")
-        parser.add_argument('--imports', dest='imports', help='Modules to import.')
-        parser.add_argument('--query', dest='query', help='The query to run.')
-        parser.add_argument('--verbose', dest='verbose', default=False, help='If given, displays extra logging messages.')
+        parser.add_argument("model", help="Model in the format app_label.ModelName")
+        parser.add_argument(
+            "--filter",
+            dest="filter",
+            help="Django ORM filter string, e.g., name=John,active=True",
+        )
+        parser.add_argument(
+            "--verbose", action="store_true", help="Show verbose output"
+        )
 
     def handle(self, *args, **options):
-        imports = options['imports']
-        query = options['query']
-        verbose = options['verbose']
-        assert imports, 'No imports specified.'
-        assert query, 'No query specified.'
-        for imp in imports.strip().split('|'):
-            imp_parts = tuple(imp.split(','))
-            if len(imp_parts) == 1:
-                cmd = ('import %s' % imp_parts)
-            elif len(imp_parts) == 2:
-                cmd = ('from %s import %s' % imp_parts)
-            elif len(imp_parts) == 3:
-                cmd = ('from %s import %s as %s' % imp_parts)
-            else:
-                raise Exception('Invalid import: %s' % (imp,))
-            if verbose:
-                print(cmd)
-            exec(cmd) # pylint: disable=exec-used
+        model_path = options["model"]
+        filter_arg = options.get("filter")
+        verbose = options["verbose"]
+
+        try:
+            app_label, model_name = model_path.split(".")
+            model = apps.get_model(app_label, model_name)
+        except (ValueError, LookupError) as e:
+            self.stderr.write(f"Invalid model: {model_path}. Error: {e}")
+            return
+
+        filters = {}
+        if filter_arg:
+            try:
+                for part in filter_arg.split(","):
+                    key, value = part.split("=")
+                    # Convert basic types
+                    if value.lower() == "true":
+                        value = True
+                    elif value.lower() == "false":
+                        value = False
+                    elif value.isdigit():
+                        value = int(value)
+                    filters[key] = value
+            except ValueError:
+                self.stderr.write(
+                    "Invalid filter format. Expected key=value,key2=value2"
+                )
+                return
+
         if verbose:
-            print(query)
-        q = eval(query, globals(), locals()) # pylint: disable=W0123
+            print(f"Filtering {model} with: {filters}")
+
+        q = model.objects.filter(**filters)
 
         job = get_current_job()
         if job:
             job.monitor_records = q.count()
             job.save()
 
-        if q.count():
-            print('%i records require attention.' % (q.count(),), file=sys.stderr)
-        else:
-            print('%i records require attention.' % (q.count(),), file=sys.stdout)
+        output = f"{q.count()} records require attention."
+        print(output, file=sys.stderr if q.exists() else sys.stdout)
